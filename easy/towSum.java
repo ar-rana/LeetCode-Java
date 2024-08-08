e9993aaf405023fc86a8da9d49af8f27cabd066c
Aryan Rana <aryan.rana2005@gmail.com>
2024-08-08 21:11:07 +0530

    LeetCode 1 - Two Sum

---
+++ easy/towSum.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int i=0;i<nums.length;i++) {
+            int val = target-nums[i];
+            if (hash.containsKey(val)){
+                return new int[] { hash.get(val), i };
+            }
+            else
+                hash.put(nums[i], i);
+        }
+        return null;
+    }
+}
