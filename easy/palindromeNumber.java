8f16f9921b63767d2bba0cec548dea07acdc9ff6
Aryan Rana <aryan.rana2005@gmail.com>
2024-08-24 23:26:28 +0530

    LeetCode 9 - Palindrome Number

---
+++ easy/palindromeNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int y = 0;
+        int z = x;
+        while(z!=0) {
+            int digit = z%10;
+            z /= 10;
+            if (y>Integer.MAX_VALUE/10 || (y == Integer.MAX_VALUE/10 && digit > 7))
+                return false;
+            y = y*10 + digit;
+        }
+        return x == y? true: false;
+    }
+}
