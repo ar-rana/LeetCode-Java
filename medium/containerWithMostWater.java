c578f2e12d47179a182ce95d5d366c537390a1a9
Aryan Rana <aryan.rana2005@gmail.com>
2024-08-25 17:56:35 +0530

    LeetCode 11 - Container With Most Water

---
+++ medium/containerWithMostWater.java
@@ -0,0 +1,19 @@
+package medium;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int max = 0;
+        while(l<r) {
+            if (height[l] <= height[r]) {
+                max = Math.max(max, (r-l)*height[l]);
+                l++;
+            } else {
+                max = Math.max(max, (r-l)*height[r]);
+                r--;
+            }
+        }
+        return max;
+    }
+}
