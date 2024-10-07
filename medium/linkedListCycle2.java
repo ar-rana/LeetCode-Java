afd9fc17f45d02d33b63f7f75691691737f130a3
Aryan Rana <aryan.rana2005@gmail.com>
2024-10-07 23:10:56 +0530

    LeetCode 142 - Linked List Cycle II

---
+++ medium/linkedListCycle2.java
@@ -0,0 +1,35 @@
+package medium;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+            if (fast == slow)
+                break;
+        }
+        if (fast==null || fast.next==null) 
+            return null;
+            
+        fast = head;
+        while (fast!=slow) {
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return fast;
+    }
+}
